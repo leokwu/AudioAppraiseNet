a3614b91cac0c6a85b2b504deab612cde020643b
leok <leok.wu@rock-chips.com>
2019-04-04 20:54:33 +0800

    implement inference

--- audioappraise_demo.py
+++ audioappraise_demo.py
@@ -1,8 +1,7 @@
 # USAGE
-# python3.6 audioappraise_demo.py --model audioappraise.model --le le.pickle --detector face_detector
+# python3.6 audioappraise_demo.py --model audioappraise.model --le le.pickle --picture f0.png
 
 # import the necessary packages
-from imutils.video import VideoStream
 from keras.preprocessing.image import img_to_array
 from keras.models import load_model
 import numpy as np
@@ -13,101 +12,53 @@ import time
 import cv2
 import os
 
-# construct the argument parse and parse the arguments
-ap = argparse.ArgumentParser()
-ap.add_argument("-m", "--model", type=str, required=True,
-	help="path to trained model")
-ap.add_argument("-l", "--le", type=str, required=True,
-	help="path to label encoder")
-ap.add_argument("-d", "--detector", type=str, required=True,
-	help="path to OpenCV's deep learning face detector")
-ap.add_argument("-c", "--confidence", type=float, default=0.5,
-	help="minimum probability to filter weak detections")
-args = vars(ap.parse_args())
 
-# load our serialized face detector from disk
-print("[INFO] loading face detector...")
-protoPath = os.path.sep.join([args["detector"], "deploy.prototxt"])
-modelPath = os.path.sep.join([args["detector"],
-	"res10_300x300_ssd_iter_140000.caffemodel"])
-net = cv2.dnn.readNetFromCaffe(protoPath, modelPath)
 
-# load the liveness detector model and label encoder from disk
-print("[INFO] loading liveness detector...")
-model = load_model(args["model"])
-le = pickle.loads(open(args["le"], "rb").read())
+def inference(config):
+	startX = 20
+	startY = 20
+	endX = 80
+	endY = 80
+	# load the audioappraise  model and label encoder from disk
+	print("[INFO] loading audioappraise ...")
+	model = load_model(config.model)
+	le = pickle.loads(open(config.le, "rb").read())
 
-# initialize the video stream and allow the camera sensor to warmup
-print("[INFO] starting video stream...")
-vs = VideoStream(src=0).start()
-time.sleep(2.0)
 
-# loop over the frames from the video stream
-while True:
-	# grab the frame from the threaded video stream and resize it
-	# to have a maximum width of 600 pixels
-	frame = vs.read()
-	frame = imutils.resize(frame, width=600)
+	# extract the face ROI and then preproces it in the exact
+	# same manner as our training data
+	frame = cv2.imread(config.picture)
+	f0_frame = cv2.resize(frame, (32, 32))
+	f0_frame = f0_frame.astype("float") / 255.0
+	f0_frame = img_to_array(f0_frame)
+	f0_frame = np.expand_dims(f0_frame, axis=0)
 
-	# grab the frame dimensions and convert it to a blob
-	(h, w) = frame.shape[:2]
-	blob = cv2.dnn.blobFromImage(cv2.resize(frame, (300, 300)), 1.0,
-		(300, 300), (104.0, 177.0, 123.0))
+	# pass the face ROI through the trained liveness detector
+	# model to determine if the f0 is "good" or "bad"
+	preds = model.predict(f0_frame)[0]
+	j = np.argmax(preds)
+	label = le.classes_[j]
 
-	# pass the blob through the network and obtain the detections and
-	# predictions
-	net.setInput(blob)
-	detections = net.forward()
-
-	# loop over the detections
-	for i in range(0, detections.shape[2]):
-		# extract the confidence (i.e., probability) associated with the
-		# prediction
-		confidence = detections[0, 0, i, 2]
-
-		# filter out weak detections
-		if confidence > args["confidence"]:
-			# compute the (x, y)-coordinates of the bounding box for
-			# the face and extract the face ROI
-			box = detections[0, 0, i, 3:7] * np.array([w, h, w, h])
-			(startX, startY, endX, endY) = box.astype("int")
-
-			# ensure the detected bounding box does fall outside the
-			# dimensions of the frame
-			startX = max(0, startX)
-			startY = max(0, startY)
-			endX = min(w, endX)
-			endY = min(h, endY)
-
-			# extract the face ROI and then preproces it in the exact
-			# same manner as our training data
-			face = frame[startY:endY, startX:endX]
-			face = cv2.resize(face, (32, 32))
-			face = face.astype("float") / 255.0
-			face = img_to_array(face)
-			face = np.expand_dims(face, axis=0)
-
-			# pass the face ROI through the trained liveness detector
-			# model to determine if the face is "real" or "fake"
-			preds = model.predict(face)[0]
-			j = np.argmax(preds)
-			label = le.classes_[j]
-
-			# draw the label and bounding box on the frame
-			label = "{}: {:.4f}".format(label, preds[j])
-			cv2.putText(frame, label, (startX, startY - 10),
-				cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
-			cv2.rectangle(frame, (startX, startY), (endX, endY),
-				(0, 0, 255), 2)
+	# draw the label and bounding box on the frame
+	label = "{}: {:.4f}".f0_frame(label, preds[j])
+	cv2.putText(frame, label, (startX, startY - 10),
+		cv2.FONT_HERSHEY_SIMPLEX, 0.5, (0, 0, 255), 2)
 
 	# show the output frame and wait for a key press
-	cv2.imshow("Frame", frame)
+	cv2.imshow("Picture", frame)
 	key = cv2.waitKey(1) & 0xFF
 
-	# if the `q` key was pressed, break from the loop
-	if key == ord("q"):
-		break
+	# do a bit of cleanup
+	cv2.destroyAllWindows()
 
-# do a bit of cleanup
-cv2.destroyAllWindows()
-vs.stop()
+if __name__ == '__main__':
+	# construct the argument parse and parse the arguments
+	parser = argparse.ArgumentParser()
+	parser.add_argument("-m", "--model", type=str, required=True,
+					help="path to trained model")
+	parser.add_argument("-l", "--le", type=str, required=True,
+					help="path to label encoder")
+	parser.add_argument("-p", "--picture", type=str, required=True,
+						help="path to f0 picture")
+	config = parser.parse_args()
+	inference(config)
